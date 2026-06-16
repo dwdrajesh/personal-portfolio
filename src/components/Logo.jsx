f6ce474bb666d765f65c0011a3e251ab8fbfb2c3
Rajesh Dawadi <rajesh.dawadi@gmail.com>
2026-06-15 21:34:40 -0700

    Add React Router

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,16 @@
-
+import {Link} from 'react-router-dom';
 import MainLogo from '../assets/logo_1.svg';
 import PropTypes from 'prop-types';
 
 const Logo = ({invert=false}) => {
     return (
+        <Link to="/">
         <div className={`flex gap-3 items-center`}>
             <img src={MainLogo} className={`w-10 h-10 ${invert ? 'brightness-0 invert' : ''}`} />
             <h2 className='text-xl font-bold'> Rajesh Dawadi </h2>
 
         </div>
+        </Link>
     )
 }
 
